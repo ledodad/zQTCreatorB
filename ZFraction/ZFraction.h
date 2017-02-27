6603f228653db8b7c9b8c8703906664693145efd
ledodad <ledodad@gmx.com>
2017-02-27 13:09:40 +0100

    first

---
+++ ZFraction/ZFraction.h
@@ -0,0 +1,24 @@
+#ifndef ZFRACTION_H
+#define ZFRACTION_H
+
+ZFraction a(4,5);      //Déclare une fraction valant 4/5
+ZFraction b(2);        //Déclare une fraction valant 2/1 (ce qui vaut 2)
+ZFraction c,d;         //Déclare deux fractions valant 0
+
+
+
+class ZFraction
+{
+public:
+    ZFration
+
+
+
+private:
+
+
+};
+
+
+
+#endif // ZFRACTION_H
